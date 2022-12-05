039274fbb264095dd73110640f2958af744cda91
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-12-05 22:19:58 +0530

    Add files via upload

---
+++ js/validate2.js
@@ -0,0 +1,96 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+   const name = document.querySelector('#name');
+   const textError = document.querySelector('.text-error');
+   name.addEventListener('input', function () {
+      if (name.value.length == 0) {
+         textError.textContent = "";
+         return;
+      }
+      try {
+         (new PersonInfo()).name = name.value;
+         textError.textContent = "";
+      } catch (e) {
+         textError.textContent = e;
+      }
+   });
+
+   const salary = document.querySelector('#salary');
+   const output = document.querySelector('.salary-output');
+   output.textContent = salary.value;
+   salary.addEventListener('input', function () {
+      output.textContent = salary.value;
+   });
+});
+
+const save = () => {
+   try {
+      let employeePayrollData = createEmployeePayroll();
+   } catch (e) {
+      return;
+   }
+}
+
+const createEmployeePayroll = () => {
+   let employeePayrollData = new PersonInfo();
+   try {
+      employeePayrollData.name = getInputValueById('#name');
+   } catch (e) {
+      setTextValue('.text-error', e);
+      throw e;
+   }
+
+   employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+   employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+   employeePayrollData.department = getSelectedValues('[name=department]');
+   employeePayrollData.salary = getInputValueById('#salary');
+   employeePayrollData.note = getInputValueById('#notes');
+   let date = getInputValueById('#day') + " " + getInputValueById('#month') + " "
+      + getInputValueById('#year');
+   employeePayrollData.start_date = Date.parse(date);
+   alert(employeePayrollData.toString());
+   return employeePayrollData;
+}
+
+const getSelectedValues = (propertyValue) => {
+   let allItems = document.querySelectorAll(propertyValue);
+   let selItems = [];
+   allItems.forEach(item => {
+      if (item.checked)
+         selItems.push(item.value);
+   });
+   return selItems;
+}
+
+const getInputValueById = (id) => {
+   let value = document.querySelector(id).value;
+   return value;
+}
+
+const resetForm = () => {
+   setValue('#name', '');
+   unsetSelectedValues('[name=profile]');
+   unsetSelectedValues('[name=gender]');
+   unsetSelectedValues('[name=department]');
+   setValue('#salary','');
+   setValue('#notes', '');
+   setValue('#day', 'Day');
+   setValue('#month', 'Month');
+   setValue('#year', 'Year');
+};
+
+const unsetSelectedValues = (propertyValue) => {
+   let allItems = document.querySelectorAll(propertyValue);
+   allItems.forEach(item => {
+      item.checked = false;
+   });
+}
+
+const setTextValue = (id, value) => {
+   const element = document.querySelector(id);
+   element.textContent = value;
+}
+
+const setValue = (id, value) => {
+   const element = document.querySelector(id);
+   element.value = value;
+}
